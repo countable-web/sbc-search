4fc9bdec574bf9fce0b3c14546a19599c7c8d8da
Aaron Ahn <aaron@countable.ca>
2020-02-18 11:03:50 -0800

    nav intialize

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,42 +1,29 @@
 <template>
-  <div id="app">
+  <v-app>
     <SbcHeader></SbcHeader>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from "vue";
 import SbcHeader from "sbc-common-components/src/components/SbcHeader.vue";
-export default {
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  name: "App",
+
   components: {
+    HelloWorld,
     SbcHeader
-  }
-};
+  },
+
+  data: () => ({
+    //
+  })
+});
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
