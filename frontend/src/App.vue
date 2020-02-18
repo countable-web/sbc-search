edee049364256b8d30e9b399a86c75eccbd0f88f
Aaron Ahn <aaron@countable.ca>
2020-02-17 23:17:52 -0800

    restrucuture project begin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <v-app class="app-wrapper">
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <SbcHeader></SbcHeader>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
-export default {};
+import SbcHeader from "sbc-common-components/src/components/SbcHeader.vue";
+export default {
+  components: {
+    SbcHeader
+  }
+};
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    <Main />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Main from "@/components/Main.vue";
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
+  name: "Home",
   components: {
-    Main
+    HelloWorld
   }
 };
 </script>
